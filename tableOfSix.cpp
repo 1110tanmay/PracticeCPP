eb9f7886c3d2c97ac371be78e5fffa72b4c0e162
1110tanmay <31026364+1110tanmay@users.noreply.github.com>
2024-02-28 22:12:17 +0530

    Create tableOfSix.cpp

---
+++ tableOfSix.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int num1 = 6;
+int num2;
+int main(){
+    int ans;
+    for (int i = 0; i<=10; i++)
+    {
+num2 = num1 * i;
+cout<<num2<<endl;
+    }
+    return 0; 
+}
